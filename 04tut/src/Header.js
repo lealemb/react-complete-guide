cfddd45ab0752831b98eb2b0f50febfcfb1ba8f4
lealembts <lealemb84@gmail.com>
2025-03-07 22:28:58 +0300

    file added

--- 04tut/src/Header.js
+++ 04tut/src/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <h1>Groceries List</h1>
+    </header>
   )
 }
- 
+
 export default Header
