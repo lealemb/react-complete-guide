1b4057cffaf1f652f19f9f69db2bbb3d7c9c1d44
lealembts <lealemb84@gmail.com>
2025-03-07 21:18:00 +0300

    add header file

--- 03tut/src/App.js
+++ 03tut/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
 
@@ -11,20 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
     </div>
   );
 }
--- 03tut/src/Header.js
+++ 03tut/src/Header.js
@@ -1,10 +1,9 @@
 import React from 'react'
-
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <h1>Groceries List</h1>
+    </header>
   )
 }
 
