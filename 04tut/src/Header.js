257cf95046eb878ad6daefc28ce36aa4d4d20c68
lealembts <lealemb84@gmail.com>
2025-03-07 22:31:49 +0300

    header file

--- 04tut/src/Header.js
+++ 04tut/src/Header.js
@@ -1,7 +1,10 @@
 import React from 'react'
 const Header = () => {
   return (
-    <header>
+    <header style={{
+      backgroundColor: 'lightblue',
+      color: 'white',
+    }}>
       <h1>Groceries List</h1>
     </header>
   )
