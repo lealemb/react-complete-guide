b67ffaa98626233c8cdca03024756c0164b71d64
lealembts <lealemb84@gmail.com>
2025-03-08 11:28:02 +0300

    onClick function

--- 05tut/src/Content.js
+++ 05tut/src/Content.js
@@ -6,11 +6,16 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
     }
+
+    const handleClick = () => {
+      console.log('You  clicked it ');
+    }
   return (
     <main>
         <p>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}> Click It</button>
     </main>
   )
 }
