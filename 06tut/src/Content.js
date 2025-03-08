083d29340f2039988015940c6a622d534c16744f
lealembts <lealemb84@gmail.com>
2025-03-08 12:42:41 +0300

    06tut added

---
+++ 06tut/src/Content.js
@@ -0,0 +1,36 @@
+
+const Content = () => {
+
+    const handleNameChange = () => {
+        const names = ['Lealem', 'Birhanu', 'Tsehay'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    }
+
+    const handleClick = () => {
+      console.log('You  clicked it ');
+    }
+
+    const handleClick2 = (name) => {
+      console.log(`${name} was Clicked`);
+    }
+
+    const handleClick3 = (e) => {
+      console.log(e.target);
+    }
+    
+  return (
+    <main>
+        <p onDoubleClick={handleClick}>
+            Hello {handleNameChange()}!
+        </p>
+        <button onClick={handleClick}> Click It</button>
+        <button onClick={()=> handleClick2('Lealem')}> Click It</button>
+        <button onClick={(e)=> handleClick3(e)}> Click It</button>
+
+
+    </main>
+  )
+}
+
+export default Content
