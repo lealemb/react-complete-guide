23289706651dbf481b4bf53f5da34bb116894b16
lealembts <lealemb84@gmail.com>
2025-03-10 00:00:38 +0300

    props

--- 08tut/src/Header.js
+++ 08tut/src/Header.js
@@ -1,10 +1,13 @@
-const Header = (props) => {
+const Header = ({title}) => {
     
   return (
     <header>
-      <h1> {props.title}</h1>
+      <h1> {title}</h1>
     </header>
   )
+}
+Header.defaultProps = {
+  title: 'Groceries List',
 }
 
 export default Header
