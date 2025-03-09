b27382969419393b9380c394d933511ed811e316
lealembts <lealemb84@gmail.com>
2025-03-10 00:09:26 +0300

    props

--- 08tut/src/Footer.js
+++ 08tut/src/Footer.js
@@ -1,7 +1,7 @@
 const Footer = ({length}) => {
   return (
         <footer>
-            <p>{length} List items</p>
+            <p>{length} List {length === 1 ? "item" : "items"}</p>
         </footer>
   )
 }
