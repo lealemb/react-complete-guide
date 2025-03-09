f4ad52849b55acdc9c7a19e9e749812319ac515c
lealembts <lealemb84@gmail.com>
2025-03-09 11:19:38 +0300

    list and keys

--- 07tut/src/Content.js
+++ 07tut/src/Content.js
@@ -2,24 +2,24 @@ import {useState} from 'react';
 const Content = () => {
       const [items, setItems] = useState ([
 
-      {
-        id: 1,
-        checked: false,
-        item: "one half pound of Coca Coverd Almonds Unsalted"
-      },
+          {
+            id: 1,
+            checked: false,
+            item: "one half pound of Coca Coverd Almonds Unsalted"
+          },
 
-      {
-        id: 2,
-        checked: false,
-        item: "item 2"
-      },
+          {
+            id: 2,
+            checked: false,
+            item: "item 2"
+          },
 
-      {
-        id: 3,
-        checked: false,
-        item: "item 3"
-      }
-    ]);
+          {
+            id: 3,
+            checked: false,
+            item: "item 3"
+          }
+        ]);
   return (
     <main>
        
