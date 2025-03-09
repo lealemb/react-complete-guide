12cbf391935cf2d97f0545ee67bba041212e89ca
lealembts <lealemb84@gmail.com>
2025-03-09 11:29:16 +0300

    list and keys

--- 07tut/src/Content.js
+++ 07tut/src/Content.js
@@ -23,7 +23,13 @@ const Content = () => {
   return (
     <main>
         <ul>
-          
+            {items.map((item) => (
+              <li className='item'>
+                <input type="checkbox"
+                checked={item.checked} />
+
+              </li>
+            ))}
         </ul>
     </main>
   )
