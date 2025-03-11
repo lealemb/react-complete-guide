dc85c3465d7046eb082c5ecbf88e82dc721302f7
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2025-03-11 21:20:25 +0300

    Update Content.js

--- 09tut/src/Content.js
+++ 09tut/src/Content.js
@@ -1,6 +1,6 @@
 import ItemList from './ItemList';
 const Content = ({items, handleCheck, handleDelete}) => {
-    return (
+    return ( 
         <main>
             {items.length ? (
                 <ItemList
