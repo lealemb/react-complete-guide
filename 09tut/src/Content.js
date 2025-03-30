22f2caea1a16918a8fcca714da50da74dd53c44a
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2025-03-30 15:49:42 +0300

    Update Content.js

--- 09tut/src/Content.js
+++ 09tut/src/Content.js
@@ -7,7 +7,7 @@ const Content = ({items, handleCheck, handleDelete}) => {
                     items={items} 
                     handleCheck={handleCheck} 
                     handleDelete={handleDelete}
-                />  
+                /> 
             ) : (  
                 <p style={{ marginTop: '2rem' }}>Your list is empty. </p>
             )}
