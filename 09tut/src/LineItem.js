587a649571d9c4fbac1bf8c2b050d7d3d2a16df1
Lealem Birhanu <101050207+lealemb@users.noreply.github.com>
2025-04-09 22:12:38 +0300

    Update LineItem.js

--- 09tut/src/LineItem.js
+++ 09tut/src/LineItem.js
@@ -1,6 +1,6 @@
 import { FaTrashAlt } from 'react-icons/fa';
 const LineItem = ({ item, handleCheck, handleDelete }) => {
-    return (
+    return ( 
         <li className="item" key={item.id}>
             <input
                 type="checkbox"
